a9ff80dad7d3cb0ce6c5d078db395c1cf8a2a00d
Vladyslav Petrenko <vladyslav533p@icloud.com>
2023-04-19 15:58:30 +0200

    Initial commit

---
+++ Currency Converter/Controller/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Vladyslav Petrenko on 19/04/2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
